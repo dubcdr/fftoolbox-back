cbaa69725c616ef78ef3da0730f71a2b18fa77c3
dubcdr <devinriley@gmai.com>
2017-05-20 11:41:13 -0500

    looking good

--- scrape/top-fantasy-season.js
+++ scrape/top-fantasy-season.js
@@ -23,41 +23,54 @@ function createPlayer(json) {
 
 }
 
+function createBio(json) {
+
+}
+
 function createSeason(json) {
 
 }
+let limit = 1;
 
-for (let i = 0; i < 400; i++) {
-  console.log('iteration: ', i);
-  osmosis.get('http://www.pro-football-reference.com/years/2016/fantasy.htm')
-    .find('#fantasy > tbody > tr')
-    .set({
-      'name': 'td > @csk',
-      'team': 'td[data-stat="team"] > a',
-      'pos': 'td[data-stat="pos"]',
-      'gp': 'td[data-stat="g"]',
-      'ps_cmp': 'td[data-stat="pass_cmp"]',
-      'ps_att': 'td[data-stat="pass_att"]',
-      'ps_yd': 'td[data-stat="pass_yds"]',
-      'ps_td': 'td[data-stat="pass_td"]',
-      "int": 'td[data-stat="pass_int"]',
-      "ru_att": 'td[data-stat="rush_att"]',
-      "ru_yd": 'td[data-stat="rush_yds"]',
-      "ru_td": 'td[data-stat="rush_td"]',
-      "rec_tg": 'td[data-stat="targets"]',
-      "rec": 'td[data-stat="rec"]',
-      "rec_yd": 'td[data-stat="rec_yds"]',
-      "rec_td": 'td[data-stat="rec_td"]',
-      "fantasy_pts": 'td[data-stat="fantasy_points"]',
-      "vbd": 'td[data-stat="vbd"]',
-      "pfr_id": 'td[data-stat="data-append-csv"]',
-    })
-    .find('td > @csk > a')
-    .follow('@href')
-    .data((response) => {
-      console.log('got response', response);
-    })
-    .log(console.log)
-    .error(console.log)
-    .debug(console.log);
-}
+osmosis.get('http://www.pro-football-reference.com/years/2016/fantasy.htm')
+  .find(`#fantasy > tbody > tr:not(.thead):limit(${limit})`)
+  .set({
+    'name': 'td[csk] > @csk',
+    'team': 'td[data-stat="team"] > a',
+    'pos': 'td[data-stat="pos"]',
+    'gp': 'td[data-stat="g"]',
+    'ps_cmp': 'td[data-stat="pass_cmp"]',
+    'ps_att': 'td[data-stat="pass_att"]',
+    'ps_yd': 'td[data-stat="pass_yds"]',
+    'ps_td': 'td[data-stat="pass_td"]',
+    "int": 'td[data-stat="pass_int"]',
+    "ru_att": 'td[data-stat="rush_att"]',
+    "ru_yd": 'td[data-stat="rush_yds"]',
+    "ru_td": 'td[data-stat="rush_td"]',
+    "rec_tg": 'td[data-stat="targets"]',
+    "rec": 'td[data-stat="rec"]',
+    "rec_yd": 'td[data-stat="rec_yds"]',
+    "rec_td": 'td[data-stat="rec_td"]',
+    "fantasy_pts": 'td[data-stat="fantasy_points"]',
+    "vbd": 'td[data-stat="vbd"]',
+    "pfr_id": 'td[data-stat="data-append-csv"]',
+  })
+  .find(`td[csk]:limit(${limit}) > a`)
+  .follow('@href')
+  .find('#meta')
+  .set({
+    'dob': 'span[itemprop="birthDate"] > @data-birth',
+    'height': 'span[itemprop="height"]',
+    'weight': 'span[itemprop="weight"]',
+    'college': 'div:nth-child(2) > p:nth-child(7) > a',
+    'twitter': 'div:nth-child(2) > p:nth-child(11) > a',
+    'draft_year': 'div:nth-child(2) > p:nth-child(10) > a:nth-child(3)'
+  })
+  .data((response) => {
+    console.log('got response', response);
+    let playerId;
+
+  })
+  .log(console.log)
+  .error(console.log)
+  .debug(console.log);
